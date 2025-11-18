a23886eff0b8bfc600eecb714110af429401ea4d
joashfan <hakuro100823@gmail.com>
2025-11-18 15:49:39 +0800

    VITE更新

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+// vite.config.js
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  // **關鍵配置：設置基底路徑**
+  // 這裡的 /PetAgeConverter/ 必須和你的 GitHub 儲存庫名稱完全一致
+  base: '/PetAgeConverter/', 
+});
